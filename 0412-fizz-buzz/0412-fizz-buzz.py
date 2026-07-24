97a8fde17d58f8766339800677de84a5a044a1f8
Tharani S <tharanisathymum@gmail.com>
2026-07-24 11:19:56 +0530

    Time: 4 ms (17.69%), Space: 13.4 MB (10.56%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        return [
+            "FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)
+            for i in range(1, n + 1)
+        ]
