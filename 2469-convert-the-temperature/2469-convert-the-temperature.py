8d796a693cf0e95b621b91f303d5e3efdc6212f7
Tharani S <tharanisathymum@gmail.com>
2026-07-24 10:51:23 +0530

    Time: 0 ms (100%), Space: 12.4 MB (20.44%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        Kelvin = celsius + 273.15
+        Fahrenheit = celsius * 1.80 + 32.00
+        return [Kelvin, Fahrenheit]
