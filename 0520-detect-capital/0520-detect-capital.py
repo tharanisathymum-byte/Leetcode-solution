5272c7da18b11ab148e8b0933ed5f95d39754676
Tharani S <tharanisathymum@gmail.com>
2026-09-25 15:11:52 +0530

    Time: 1 ms (13.96%), Space: 12.5 MB (16.84%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        if word.isupper() or word.islower() or word.istitle():
+            return True
+        else:
+            return False
+        
