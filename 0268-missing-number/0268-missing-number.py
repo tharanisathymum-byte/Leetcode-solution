b950a7b58443732723390d19297c0fa24102bbfe
Tharani S <tharanisathymum@gmail.com>
2026-09-25 14:59:13 +0530

    Time: 0 ms (100%), Space: 13.2 MB (94.43%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def missingNumber(self, nums):
-            n = len(nums)
-            a=(n*(n+1))//2
-            b=sum(nums)
-            return a-b
+        n = len(nums)
+        a=(n*(n+1))//2
+        b=sum(nums)
+        return a-b
