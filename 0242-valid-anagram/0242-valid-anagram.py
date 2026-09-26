24767e7d3ce2dc6111b7738b617c82352d05ff42
Tharani S <tharanisathymum@gmail.com>
2026-09-26 14:30:51 +0530

    Time: 5 ms (98.27%), Space: 12.4 MB (86.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        # In case of different length of thpse two strings...
         if len(s) != len(t):
             return False
-        for idx in set(s):
-            # Compare s.count(l) and t.count(l) for every index i from 0 to 26...
-            # If they are different, return false...
-            if s.count(idx) != t.count(idx):
+        for i in set(s):
+            if s.count(i) != t.count(i):
                 return False
-        return True     # Otherwise, return true...
+        return True     
